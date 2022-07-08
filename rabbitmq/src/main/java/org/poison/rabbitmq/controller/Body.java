1181a56505533a4491b18dbc3e22078aed8719db
banzh <1147092872@qq.com>
2022-07-09 02:57:05 +0800

    rabbit init

---
+++ rabbitmq/src/main/java/org/poison/rabbitmq/controller/Body.java
@@ -0,0 +1,9 @@
+package org.poison.rabbitmq.controller;
+
+import lombok.Data;
+
+@Data
+public class Body {
+    String a;
+    String b;
+}
---
+++ rabbitmq/src/main/java/org/poison/rabbitmq/controller/TestController.java
@@ -0,0 +1,21 @@
+package org.poison.rabbitmq.controller;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @SneakyThrows
+    @PostMapping(value = "")
+    public String test(@RequestBody Body abc) {
+
+        return abc.a;
+    }
+}
