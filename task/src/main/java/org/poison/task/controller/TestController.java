b3baaac0edd331082000445cc06bb5db0b36b8bb
banzh <1147092872@qq.com>
2022-09-02 19:08:18 +0800

    saving

--- task/src/main/java/org/poison/task/controller/TestController.java
+++ task/src/main/java/org/poison/task/controller/TestController.java
@@ -8,9 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.poison.merge.ack.UseAck;
 import org.poison.task.distinct.SetHandler;
 import org.poison.task.task.Task;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import javax.annotation.Resource;
 
@@ -41,8 +47,41 @@ public class TestController {
         }
     }
 
+
+    @Resource
+    @Qualifier("testThreadPool")
+    private ExecutorService testExecutor;
+    
     @PostMapping(value = "get")
     public void get() {
-        setHandler.handleTask();
+        List<String> list = new ArrayList<>();
+        for (int i =0 ;i<100;i++){
+            list.add("abc"+i);
+        }
+        
+        List<Future<?>> futureList = new ArrayList<>();
+        for (String s : list) {
+            Future<?> future = testExecutor.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                log.info(s);
+            });
+            futureList.add(future);
+        }
+        futureList.forEach(it -> {
+            try {
+                it.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        
+        log.info("finish");
+        
     }
 }
