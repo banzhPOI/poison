350209a614ebb60f226223c253ea52a38709f3bf
banzh <1147092872@qq.com>
2022-11-17 09:41:24 +0800

    saving

--- compactqueue/src/main/java/org/poison/compactqueue/ack/AopAck.java
+++ compactqueue/src/main/java/org/poison/compactqueue/ack/AopAck.java
@@ -1,18 +1,36 @@
 package org.poison.compactqueue.ack;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import lombok.extern.slf4j.Slf4j;
 
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface AopAck {
-    /**
-     * 
-     * @return
-     */
-    String value();
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.poison.compactqueue.Compaction;
+import org.redisson.api.RMapCache;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@Aspect
+@Component
+public class AopAck {
+
+    @Resource
+    private Compaction compaction;
+RMapCache
+    @Pointcut("@annotation(org.poison.compactqueue.ack.UseAck)")
+    public void aopPoint() {
+    }
+
+    @Around("aopPoint()")
+    public void aroundAop(ProceedingJoinPoint pj) throws Throwable {
+        try {
+            pj.proceed();
+        } catch (Exception e) {
+            log.warn("task handle failed:", e);
+//            setMerger.add();
+        }
+    }
 }
