2bf43163f6b03ebddd7f75ae954b4da4c22133f1
banzh <1147092872@qq.com>
2022-12-03 16:09:50 +0800

    es starter

--- starter/elasticsearch-starter/src/main/java/org/poison/starter/elasticsearch/ElasticsearchClientConfig.java
+++ starter/elasticsearch-starter/src/main/java/org/poison/starter/elasticsearch/ElasticsearchClientConfig.java
@@ -18,7 +18,6 @@ import org.springframework.context.annotation.Configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-@Configuration
 public class ElasticsearchClientConfig {
 
     private final ElasticsearchConfig config;
