9d306dcecbf872bf54ffc898eaca1537632f464b
banzh <1147092872@qq.com>
2022-12-03 13:16:51 +0800

    snowflake

--- starter/snowflake-starter/src/main/java/org/poison/starter/snowflake/SnowflakeConfig.java
+++ starter/snowflake-starter/src/main/java/org/poison/starter/snowflake/SnowflakeConfig.java
@@ -1,26 +1,26 @@
 package org.poison.starter.snowflake;
 
 
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.redisson.api.RedissonClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 
+@Slf4j
 public class SnowflakeConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(SnowflakeConfig.class);
-
+    @Resource
+    private RedissonClient redissonClient;
 
     private static final long DATACENTER_ID = 1;
     private static final int MAX_WORKER_COUNT = 3;
-    private static final String REDIS_WORK_ID_KEY = "genie-ims-redis-work-id-key";
+    private static final String REDIS_WORK_ID_KEY = "worker-id-key";
     private final long workerId;
 
-
-    SnowflakeConfig(StringRedisTemplate redisTemplate) {
-        this.workerId = initWorkerId(redisTemplate);
+    SnowflakeConfig() {
+        this.workerId = initWorkerId();
     }
 
     /**
@@ -32,17 +32,13 @@ public class SnowflakeConfig {
         return new SnowflakeIdWorker(workerId, DATACENTER_ID);
     }
 
-
     /**
      * 获取当前服务实例编号，作为分布式Id生成器机器位
      */
-    private int initWorkerId(StringRedisTemplate redisTemplate) {
+    private int initWorkerId() {
         int value;
         try {
-            Long incrementValue = redisTemplate.opsForValue().increment(REDIS_WORK_ID_KEY);
-            if (incrementValue == null) {
-                throw new IllegalAccessException("Increase number is null.[redisTemplate.opsForValue().increment(" + REDIS_WORK_ID_KEY + ")]");
-            }
+            Long incrementValue = redissonClient.getAtomicLong(REDIS_WORK_ID_KEY).addAndGet(1L);
             value = incrementValue.intValue() % MAX_WORKER_COUNT;
         } catch (Exception e) {
             value = RandomUtils.nextInt(0, MAX_WORKER_COUNT);
