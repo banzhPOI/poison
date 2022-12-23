6732639d41b72e22975006014a4b62c32af21d3e
zihan.ban <1147092872@qq.com>
2022-12-24 01:46:19 +0800

    控制第三方bean的加载顺序

--- starter/mybatis-starter/src/main/java/org/poison/starter/mybatis/MybatisConfig.java
+++ starter/mybatis-starter/src/main/java/org/poison/starter/mybatis/MybatisConfig.java
@@ -1,6 +1,22 @@
 package org.poison.starter.mybatis;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@AutoConfigureBefore(MybatisAutoConfiguration.class)
 public class MybatisConfig {
 
-
+    @Bean
+    SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return bean.getObject();
+    }
 }
