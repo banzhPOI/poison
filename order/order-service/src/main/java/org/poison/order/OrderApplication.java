b4ec023444866e1b25e4cb8ac872d0123a953081
zihan.ban <1147092872@qq.com>
2023-03-23 19:52:32 +0800

    接入nacos

--- order/order-service/src/main/java/org/poison/order/OrderApplication.java
+++ order/order-service/src/main/java/org/poison/order/OrderApplication.java
@@ -3,6 +3,7 @@ package org.poison.order;
 import org.poison.starter.cloud.enableAnnotations.EnableFeignCloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
