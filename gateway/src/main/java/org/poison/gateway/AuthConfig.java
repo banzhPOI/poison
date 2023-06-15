f889d3dd62671cff3c730cf376db9edd108a4d44
banzh <1147092872@qq.com>
2023-06-16 01:44:37 +0800

    gateway增加注册url过滤

--- gateway/src/main/java/org/poison/gateway/AuthConfig.java
+++ gateway/src/main/java/org/poison/gateway/AuthConfig.java
@@ -1,9 +1,8 @@
 package org.poison.gateway;
 
+import cn.dev33.satoken.filter.SaFilterAuthStrategy;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.Resource;
 import org.poison.common.constant.GlobalConstant;
 import org.poison.common.response.Response;
 import org.springframework.context.annotation.Bean;
@@ -17,8 +16,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AuthConfig {
 
-    @Resource
-    private ObjectMapper objectMapper;
 
     // 注册 Sa-Token全局过滤器
     @Bean
@@ -29,18 +26,21 @@ public class AuthConfig {
                 // 开放地址
                 .addExclude("/favicon.ico")
                 // 鉴权方法：每次访问进入
-                .setAuth(obj -> {
-                    // 登录校验 -- 拦截所有路由，并排除/iam/login 用于开放登录
-                    SaRouter.match("/**", GlobalConstant.LOGIN_URL, r -> StpUtil.checkLogin());
-                    SaRouter.match("/**", GlobalConstant.REGISTER_URL, r -> StpUtil.checkLogin());
+                .setAuth(new SaFilterAuthStrategy() {
+                    @Override
+                    public void run(Object obj) {
+                        // 登录校验 -- 拦截所有路由，并排除/iam/login 用于开放登录
+                        SaRouter.match("/**", GlobalConstant.LOGIN_URL, r -> StpUtil.checkLogin())
+                                .match("/**", GlobalConstant.REGISTER_URL, r -> StpUtil.checkLogin());
 
-                    // 权限认证 -- 不同模块, 校验不同权限
+                        // 权限认证 -- 不同模块, 校验不同权限
 //                    SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
 //                    SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
 //                    SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
 //                    SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
 
-                    // 更多匹配 ...  */
+                        // 更多匹配 ...  */
+                    }
                 })
                 // 异常处理方法：每次setAuth函数出现异常时进入
                 .setError(e -> Response.fail(e.getMessage()));
